3b55a047cdfafbe3a8378406dc10d3ad32f14890
Shubham Sharma <shubhamsh3099@gmail.com>
2026-02-26 19:13:30 +0530

    Fix navbar

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -2,23 +2,9 @@ import getJobsFromStorage from "./storage.js";
 
 const jobsCountEl = document.querySelector(".count");
 const jobsContainerEl = document.querySelector(".job-container");
+const clearBtnEl = document.querySelector(".clear-btn");
 
-// let jobs = getJobsFromStorage();
-let jobs = [
-    {
-        name: "Comp",
-        description: "description goes here",
-        date: "Feb 25, 2026",
-        link: "https://google.com",
-    },
-    {
-        name: "Comp",
-        description: "description goes here",
-        date: "Feb 25, 2026",
-        link: "https://google.com",
-    },
-];
-
+let jobs = getJobsFromStorage();
 renderJobs();
 
 function renderJobs () {
@@ -31,7 +17,6 @@ function renderJobs () {
                 <time datetime="2026-02-25"> ${job.date} </time>
                 <div class="bottom-jobItem-container">
                     <a href="${job.link}" target="_blank">View Job Posting</a>
-                    <button>Delete</button>
                 </div>
             </div> 
         `
@@ -40,3 +25,8 @@ function renderJobs () {
     jobsCountEl.textContent = jobs.length;
 }
 
+clearBtnEl.addEventListener("click", () => {
+    localStorage.clear();
+    jobsContainerEl.innerHTML = "";
+    jobsCountEl.textContent = 0;
+});
